e87280d57f89f9123b74ee57a1522ade02a61f9b
Jen C <jenjch8@gmail.com>
2020-01-04 05:33:27 -0600

    created static HTML/CSS, added to generateHTML with template literals placeholder data

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+const pdf = require('html-pdf');
+const generateHTML = require ('./generateHTML');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const questions = [
-  
+{
+    type:"input",
+    name: "github"
+    message: "What is your GitHub username?"
+},
+{ 
+    type: "list",
+    name: "color",
+    message: "What color do you choose?",
+    choices: ["green", "blue", "pink", "red"]
+} 
 ];
 
 function writeToFile(fileName, data) {
@@ -9,3 +28,33 @@ function writeToFile(fileName, data) {
 function init() {
 
 init();
+
+// axios
+// const fs = require("fs");
+// const axios = require("axios");
+// const inquirer = require("inquirer");
+
+// inquirer
+//   .prompt({
+//     message: "Enter your GitHub username:",
+//     name: "username"
+//   })
+//   .then(function({ username }) {
+//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//     axios.get(queryUrl).then(function(res) {
+//       const repoNames = res.data.map(function(repo) {
+//         return repo.name;
+//       });
+
+//       const repoNamesStr = repoNames.join("\n");
+
+//       fs.writeFile("repos.txt", repoNamesStr, function(err) {
+//         if (err) {
+//           throw err;
+//         }
+
+//         console.log(`Saved ${repoNames.length} repos`);
+//       });
+//     });
+//   });
